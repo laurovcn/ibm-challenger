0e7a0a07c2d08e4effdd22986646826343cd412c
lvneto <lauro.neto.1995@gmail.com>
2021-08-03 09:24:46 -0300

    add dirname in entity app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
-import { Book } from './books/entities/book.entity';
+
 
 @Module({
   imports: [ TypeOrmModule.forRoot({
@@ -13,7 +12,9 @@ import { Book } from './books/entities/book.entity';
     username: 'dev',
     password: 'Dev612@dev',
     database: 'dev',
-    entities: [User, Book],
+    entities: [
+      __dirname + '/**/**/*.entity{.ts,.js}',
+  ],
     synchronize: true,
   }), UsersModule, BooksModule,], 
 })
